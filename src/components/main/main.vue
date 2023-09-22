676eb96abb8044ce092685f853529e60d24c2130
Guaishou233 <94284422+Guaishou233@users.noreply.github.com>
2023-09-22 20:50:04 +0800

    初版

--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -3,7 +3,9 @@
         <input type="file" @change="handleFileUpload" accept="image/*"/>
         <button @click="recognizeAllImages">识别文本</button>
         <img v-for="url in splitImages" :src="url" :key="url" />
-        <div>{{ recognizedText }}</div>
+        <button @click="calculate">计算</button>
+        <div>{{red_operatorsArray}}{{ red_numbersArray }}</div>
+        <div>{{blue_operatorsArray}}{{ blue_numbersArray }}</div>
     </div>
 </template>
 
@@ -15,10 +17,14 @@
         data() {
             return {
                 worker: null,
-                recognizedText: '',
+                recognizedText: [],
                 file: '',
                 splitImages:[],
-                splitNum:[]
+                splitNum:[],
+                red_operatorsArray:[],
+                blue_operatorsArray:[],
+                red_numbersArray:[],
+                blue_numbersArray:[],
             };
         },
         //与处理图像，先分割图像为多个待识别小图片
@@ -47,8 +53,36 @@
                 }
 
                 const results = await Promise.all(promises);
-                this.recognizedText = results.map(r => r.data.text).join(' ');
-                console.log(this.recognizedText)
+                let addToRedArray = true; // 初始时添加到红色数组
+                // 使用map函数遍历results
+                results.map(r => {
+                    // 使用正则表达式匹配运算符+空格+数字+换行符的模式
+                    const regex = /([-+*/])\s*(\d+)\n/g;
+
+                    // 在文本中查找匹配的模式
+                    // 在文本中查找匹配的模式
+                    let match;
+
+                    while ((match = regex.exec(r.data.text)) !== null) {
+                        const operator = match[1]; // 运算符
+                        const number = parseInt( match[2],10);   // 数字
+                        if (addToRedArray) {
+                            this.red_operatorsArray.push(operator);
+                            this.red_numbersArray.push(number);
+                        } else {
+                            this.blue_operatorsArray.push(operator);
+                            this.blue_numbersArray.push(number);
+                        }
+
+                        // 切换到下一个数组，实现奇偶分离
+                        addToRedArray = !addToRedArray;
+                    }
+
+                });
+
+// 现在operatorsArray包含了运算符号，numbersArray包含了数字
+                console.log("Operators:", this.red_operatorsArray);
+                console.log("Numbers:", this.red_numbersArray);
                 for (let worker of workers) {
                     await worker.terminate();
                 }
@@ -95,8 +129,22 @@
                         //this.changeGray(context, canvas)
                         //分割图片
                         this.spliteImg(context,canvas,image, [
-                            { x: 1430, y: 250, width: 75, height: 35 }, // 第一个分割位置和大小
-                            { x: 1550, y: 250, width: 75, height: 35 }, // 第二个分割位置和大小
+                            { x: 1435, y: 260, width: 70, height: 30 }, // 第一个分割位置和大小
+                            { x: 1560, y: 250, width: 70, height: 30 }, // 第二个分割位置和大小
+                            { x: 1430, y: 360, width: 70, height: 30 },
+                            { x: 1560, y: 360, width: 70, height: 30 },
+                            { x: 1430, y: 470, width: 70, height: 30 },
+                            { x: 1550, y: 470, width: 70, height: 30 },
+                            { x: 1430, y: 570, width: 70, height: 30 },
+                            { x: 1550, y: 570, width: 70, height: 30 },
+                            { x: 1430, y: 672, width: 70, height: 30 },
+                            { x: 1550, y: 672, width: 70, height: 30 },
+                            { x: 1430, y: 774, width: 70, height: 30 },
+                            { x: 1550, y: 774, width: 70, height: 30 },
+                            { x: 1430, y: 880, width: 70, height: 30 },
+                            { x: 1550, y: 880, width: 70, height: 30 },
+                            { x: 1430, y: 980, width: 70, height: 30 },
+                            { x: 1550, y: 980, width: 70, height: 30 },
                             // 在这里添加更多的分割位置和大小
                         ]);
                     };
@@ -151,8 +199,66 @@
                 // 将修改后的像素数据放回 Canvas
                 context.putImageData(imageData, 0, 0);
 
+            },
+//逐个识别，然后计算
+            //计算方法设计
+            calculate(){
+
+                function cal(input, op, num) {
+                    switch (op) {
+                        case '+':
+                            input += num;
+                            break;
+                        case '-':
+                            input -= num;
+                            break;
+                        case '*':
+                            input *= num;
+                            break;
+                        case '/':
+                            input = Math.floor(input / num) + (input % num === 0 ? 0 : 1);
+                            break;
+                        default:
+                            break;
+                    }
+                    return input;
+                }
+
+                function log(op, nums, input, select) {
+                    let i = 0;
+                    while (select > 0) {
+                        if (select & 1) {
+                            console.log(`${input} ${op[i]} ${nums[i]} = ${input}`);
+                            cal(input, op[i], nums[i]);
+                        }
+                        i++;
+                        select >>= 1;
+                    }
+                }
+
+                function solve(op, nums, input, output, select) {
+                    let i = 0;
+                    while (select > 0) {
+                        if (select & 1) {
+                            input = cal(input, op[i], nums[i]);
+                        }
+                        i++;
+                        select >>= 1;
+                    }
+                    return  input === output;
+                }
+
+                for (let i = 0; i < (1 << 8); i++) {
+                    if (solve(this.red_operatorsArray, this.red_numbersArray, 80, 28, i) && solve(this.blue_operatorsArray, this.blue_numbersArray, 80, 90, i)) {
+                        console.log("Red");
+                        log(this.red_operatorsArray, this.red_numbersArray, 80, i);
+                        console.log("Blue");
+                        log(this.blue_operatorsArray, this.blue_numbersArray, 80, i);
+                        console.log("-------------");
+                    }
+                }
+
             }
         },
-
     };
 </script>
